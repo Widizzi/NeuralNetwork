cd56c5cda2a0e717f214869c4e2a61579fd4cfbb
Florian Wiederkehr <florian@widi.ch>
2020-10-03 13:11:20 +0200

    CLI
    The ProgressBar class can add information to the Console by showing the progress of learning.

---
+++ src/library/interface/cli/ProgressBar.py
@@ -0,0 +1,38 @@
+class ProgressBar:
+
+    def progressBar(self, iterations, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+        """
+        Call in a loop to create terminal progress bar
+        @params:
+            iteration   - Required  : current iteration (Int)
+            total       - Required  : total iterations (Int)
+            prefix      - Optional  : prefix string (Str)
+            suffix      - Optional  : suffix string (Str)
+            decimals    - Optional  : positive number of decimals in percent complete (Int)
+            length      - Optional  : character length of bar (Int)
+            fill        - Optional  : bar fill character (Str)
+            printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+        """
+        self.total = iterations
+        self.iterations = iterations
+        self.prefix = prefix
+        self.suffix = suffix
+        self.decimals = decimals
+        self.length = length
+        self.fill = fill
+        self.printEnd = printEnd
+
+
+        self.printProgressBar(0)
+
+
+    def printProgressBar(self, iteration):
+        percent = ("{0:." + str(self.decimals) + "f}").format(100 * (iteration / float(self.total)))
+        filledLength = int(self.length * iteration // self.total)
+        bar = self.fill * filledLength + '-' * (self.length - filledLength)
+        print(f'\r{self.prefix} |{bar}| {percent}% {self.suffix}', end = self.printEnd)
+
+        if filledLength == self.length:
+            print()
+            print()
+        
