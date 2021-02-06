3c242b957e07d35b2fd7ca26d76da8175f1835d0
Florian <florian@widi.ch>
2021-02-06 19:54:18 +0100

    Genetic-XOR-Project
    To illustrate the genetic network a new project has been added. It can be started from the command line in the root folder

---
+++ src/__main__.py
@@ -0,0 +1,17 @@
+import sys
+from enum import Enum
+
+import src.bin.genetic_xor.Main as genetic_xor
+
+def switch(project):
+    Projects = {
+        # add the main func of projects to this dictionary to run them with command line arguments
+        "xor": genetic_xor.run
+    }
+    return Projects.get(project, "No such project")
+
+if __name__ == '__main__':
+    if switch(sys.argv[1]) == "No such project":
+        print("No such project")
+    else:
+        switch(sys.argv[1])()
